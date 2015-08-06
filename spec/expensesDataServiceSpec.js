79562cb47d3df52afb2eb4c7f8c0bc64a7af75cc
Michael Bowlin <michaelbowlin@Michaels-MacBook-Pro.local>
2015-08-06 08:00:07 -0600

    Refactored expensesDataServiceSpec.js to have a nested describe. Also added a 'dinner' expense item.

--- spec/expensesDataServiceSpec.js
+++ spec/expensesDataServiceSpec.js
@@ -2,9 +2,6 @@
 describe('expensesDataService', function(){
 
     beforeEach(module('app'));
-    beforeEach(function(){
-       jasmine.addMatchers(customMatchers);
-    });
 
     it('should return three expense items', inject(function(expensesDataService) {
         expect(expensesDataService.getExpenses().length).toBe(3);
@@ -18,11 +15,29 @@ describe('expensesDataService', function(){
 
     }));
 
-    // custom matcher
-    it('taxi should be a reasonable expense', function(){
-        var taxi = new ExpenseItem('Taxi','To airport','89.95');
-        expect(taxi).toBeAResonableExpense();
+    // nested describe
+    describe('reasonable expenses', function(){
+
+        var taxi;
+        var dinner;
+
+        beforeEach(function(){
+            jasmine.addMatchers(customMatchers);
+        });
+
+        beforeEach(function(){
+           taxi = new ExpenseItem('Taxi','To airport','89.95');
+            dinner = new ExpenseItem('Dinner','Dinner with John and Ward',189.50);
+        });
+
+        // custom matcher
+        it('taxi should be a reasonable expense', function(){
+            expect(taxi).toBeAResonableExpense();
+        });
+
+        it('dinner should not be a reasonable expense', function(){
+           expect(dinner).not.toBeAResonableExpense();
+        });
+
     });
-
-
 });
